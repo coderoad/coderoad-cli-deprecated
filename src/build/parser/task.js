33afbb0998263f468c41be6e5d86703d46fc81c9
ShMcK <shawn.j.mckay@gmail.com>
2016-02-03 22:00:51 -0800

    parser task updates

--- src/build/parser/task.js
+++ src/build/parser/task.js
@@ -3,33 +3,57 @@ var Match = require('./match');
 var chapter_1 = require('./chapter');
 var page_1 = require('./page');
 var actions_1 = require('./actions');
+function bracketTracker(line) {
+    var l = (line.match(/\(/g) || []).length;
+    var r = (line.match(/\)/g) || []).length;
+    return l - r;
+}
 function task(result, lines, index) {
     result.chapters[index.chapter].pages[index.page].tasks.push({
         description: Match.task(lines[0])
     });
     index.task += 1;
     var inCodeBlock = false;
+    var currentAction = null;
+    var bracketCount = 0;
     for (var i = 1; i < lines.length; i++) {
         var line = lines[i];
-        if (!!Match.codeBlock(line)) {
-            inCodeBlock = !inCodeBlock;
+        var finishedAction = (bracketTracker(line) + bracketCount) === 0;
+        if (!!currentAction && !finishedAction) {
+            currentAction += line;
+            bracketCount += bracketTracker(line);
         }
-        if (!inCodeBlock) {
+        else if (!!currentAction) {
+            result = actions_1.default(result, currentAction, index);
+            currentAction = null;
+            bracketCount = 0;
+        }
+        else {
             var isAction = Match.isAction(line);
-            if (!!isAction) {
-                result = actions_1.default(result, line, index);
+            if (!isAction && !!Match.codeBlock(line)) {
+                inCodeBlock = !inCodeBlock;
             }
-            else if (!!Match.task(line)) {
-                return task(result, lines.slice(i), index);
-            }
-            else if (!!Match.page(line)) {
-                return page_1.default(result, lines.slice(i), index);
-            }
-            else if (!!Match.chapter(line)) {
-                return chapter_1.default(result, lines.slice(i), index);
-            }
-            else {
-                result.chapters[index.chapter].pages[index.page].tasks[index.task].description += line + '\n';
+            if (!inCodeBlock) {
+                if (!!isAction) {
+                    currentAction = line;
+                    bracketCount = bracketTracker(line);
+                    if (bracketCount === 0) {
+                        result = actions_1.default(result, currentAction, index);
+                        currentAction = null;
+                    }
+                }
+                else if (!!Match.task(line)) {
+                    return task(result, lines.slice(i), index);
+                }
+                else if (!!Match.page(line)) {
+                    return page_1.default(result, lines.slice(i), index);
+                }
+                else if (!!Match.chapter(line)) {
+                    return chapter_1.default(result, lines.slice(i), index);
+                }
+                else {
+                    result.chapters[index.chapter].pages[index.page].tasks[index.task].description += line + '\n';
+                }
             }
         }
     }
