0e5a975e59d3814ff0d7108d88a2d0aa75eab5a2
ShMcK <shawn.j.mckay@gmail.com>
2016-02-29 17:28:29 -0800

    update create with improved file paths

--- src/create/setup/tutorial/tutorial.md
+++
@@ -1,9 +0,0 @@
-# Project Title
-Project description.
-
-## Chapter One Title
-Chapter one description.
-
-@import('tutorial/1/01/page-one')
-
-@import('tutorial/1/02/page-two')
--- src/create/write-demo.ts
+++ src/create/write-demo.ts
@@ -5,7 +5,7 @@ import {fileExists} from '../tools/file';
 
 function createFile(pathToFile: string): void {
   if (!fileExists(pathToFile)) {
-    let inputPath = path.join(__dirname, 'setup', pathToFile);
+    let inputPath = path.join(__dirname, '..', '..', 'setup', pathToFile);
     let test = fs.readFileSync(inputPath, 'utf8');
     fs.writeFileSync(pathToFile, test, 'utf8');
   }
@@ -19,17 +19,19 @@ function createFolder(pathToFolder: string): void {
 
 export function createTutorialMd(): void {
   createFolder('tutorial');
-  createFile('tutorial/tutorial.md');
-  createFolder('tutorial/1');
-  createFolder('tutorial/1/01');
-  createFile('tutorial/1/01/page-one.md');
-  createFolder('tutorial/1/02');
-  createFile('tutorial/1/02/page-two.md');
+  createFile(path.join('tutorial', 'tutorial.md'));
+  createFolder(path.join('tutorial', '1'));
+  createFolder(path.join('tutorial', '1', '01'));
+  createFile(path.join('tutorial', '1', '01', 'page-one.md'));
+  createFolder(path.join('tutorial', '1', '02'));
+  createFile(path.join('tutorial', '1', '02', 'page-two.md'));
+  createFolder(path.join('tutorial', 'common'));
+  createFile(path.join('tutorial', 'common', 'loadJS.js'));
 }
 
 export function createPackageJson(name: string): void {
   if (!fileExists('package.json')) {
-    let inputPath = path.join(__dirname, './setup/package.json');
+    let inputPath = path.join(__dirname, '..', '..', 'setup', 'package.json');
     let packageJson = JSON.parse(fs.readFileSync(inputPath, 'utf8'));
     packageJson.name = 'coderoad-' + name
     let packageJsonString = JSON.stringify(packageJson, null, 2);
@@ -38,8 +40,8 @@ export function createPackageJson(name: string): void {
 }
 
 export function createTestFiles():void {
-  createFile('tutorial/1/01/01.spec.js');
-  createFile('tutorial/1/01/02.spec.js');
-  createFile('tutorial/1/02/01.spec.js');
-  createFile('tutorial/1/02/02.spec.js');
+  createFile(path.join('tutorial', '1', '01', '01.spec.js'));
+  createFile(path.join('tutorial', '1', '01', '02.spec.js'));
+  createFile(path.join('tutorial', '1', '02', '01.spec.js'));
+  createFile(path.join('tutorial', '1', '02', '02.spec.js'));
 }
