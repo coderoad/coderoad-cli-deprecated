86cc2134e4f45151222b9aec140889a75842b0c7
ShMcK <shawn.j.mckay@gmail.com>
2016-03-12 16:16:26 -0800

    lint output, v0.3.27

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,9 @@
 All notable changes to this project will be documented in this file.
 This project adheres to [Semantic Versioning](http://semver.org/).
 
+## [0.3.27] - 2016-03-12
+- lint output and show errors/warnings
+
 ## [0.3.14] - 2016-02-26
 ### Added
 - updated create templates
--- src/build/validators.ts
+++ src/build/validators.ts
@@ -1,4 +1,5 @@
 import * as chalk from 'chalk';
+import {isValidJSON, hasProjectInfo, hasPage, lintOutput} from './lint';
 
 export function filePath(filePath: string): void {
   if (!filePath) {
@@ -17,36 +18,6 @@ export function result(text: string): boolean {
   let jsonObject = JSON.parse(text);
   hasProjectInfo(jsonObject);
   hasPage(jsonObject);
+  lintOutput(jsonObject);
   return true;
-}
-
-function isValidJSON(text: string): void {
-  if (!/^[\],:{}\s]*$/.test(text.replace(/\\["\\\/bfnrtu]/g, '@').
-    replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').
-    replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
-    console.log(chalk.red(`
-      Something went wrong. There seems to be an error in ${filePath}.
-      `));
-    process.exit(1); // fail
-  }
-}
-
-function hasProjectInfo(json: CR.Output): void {
-  let validTitle = json.project.title.length > 0,
-    validDescription = json.project.description.length > 0;
-  if (!(validTitle && validDescription)) {
-    console.log(chalk.red(`
-      Your tutorial is missing basic project information. Check the project title & description.
-      `));
-    process.exit(1); // fail
-  }
-}
-
-function hasPage(json: CR.Output): void {
-  if (!(json.chapters[0].pages.length > 0 && !!json.chapters[0].pages[0].title)) {
-    console.log(chalk.red(`
-      Your tutorial requires at least one page.
-      `));
-    process.exit(1); // fail
-  }
 }
