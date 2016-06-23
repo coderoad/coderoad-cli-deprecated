f72c43fddd2344153e5ad61858296f1e61a6366d
ShMcK <shawn.j.mckay@gmail.com>
2016-06-23 15:09:50 -0700

    fix packageJson validate content

--- src/validate/packageJson.ts
+++ src/validate/packageJson.ts
@@ -2,59 +2,59 @@ const pJKeys: PJKeys[] = [{
   name: 'name',
   validate: name => !!name.match(/^coderoad-[A-Za-z0-9\-]+$/),
   msg: 'must be kebabcased and start with "coderoad"',
-  example: 'coderoad-tutorial-name',
+  example: '"coderoad-tut-name"',
 }, {
     name: 'version',
     validate: version => !!version.match(/^[0-9]+\.[0-9]+\.[0-9]+$/),
     msg: 'must be 3 numbers separated by dots',
-    example: '0.1.0',
+    example: '"0.1.0"',
   }, {
     name: 'main',
     validate: main => main === 'coderoad.json',
     msg: 'must point to coderoad.json',
-    example: 'coderoad.json',
+    example: '"coderoad.json"',
   }, {
     name: 'description',
     validate: desc => typeof desc === 'string' && desc.length > 3,
     msg: 'must be long enough to describe a package',
-    example: 'CodeRoad tutorial on ES2015 new features.'
+    example: '"CodeRoad tutorial on ES2015 new features."'
   }, {
     name: 'keywords',
     validate: keywords => Array.isArray(keywords) && !!keywords.length && keywords.includes('coderoad'),
     msg: 'must be an array containing "coderoad"',
-    example: '["coderoad", "tutorial", "js"]',
+    example: '[\n"coderoad",\n"tutorial",\n"js"\n]',
   }, {
     name: 'author',
     validate: author => typeof author === 'string' && author.length > 2,
     msg: 'must have an author name and optional email',
-    example: 'Shawn McKay <me@email.com> (http://blog)',
+    example: '"Shawn McKay <me@email.com> (http://blog)"',
   }, {
     name: 'config',
     validate: config => typeof config === 'object',
     msg: 'must be an object',
-    example: '"config": {"language": "JS", "runner": "mocha-coderoad"}',
+    example: '{\n"language": "JS",\n"runner": "mocha-coderoad"\n}',
   }, {
     name: 'files',
     validate: files => Array.isArray(files) && files.includes('coderoad.json') && files.includes('tutorial'),
     msg: 'must be an array including "coderoad.json" & "tutorial"',
-    example: '["coderoad.json", "tutorial"]',
+    example: '[\n"coderoad.json",\n "tutorial"\n]',
   }, {
     name: 'engines',
     validate: engines => typeof engines === 'object' && !!engines.node && !!engines.node.match(/^(>=)?[0-9]+/),
     msg: 'must specify a valid node version',
-    example: '"engines": { "node": ">=0.10.3"}',
+    example: '{\n "node": ">=0.10.3"\n}',
   }, {
     name: 'language',
     config: true,
     validate: lang => typeof lang === 'string' && !!lang.length,
     msg: 'must specify a programming language',
-    example: 'JS',
+    example: '"JS"',
   }, {
     name: 'runner',
     config: true,
     validate: runner => typeof runner === 'string' && !!runner.length,
     msg: 'must specify a test runner',
-    example: 'mocha-coderoad',
+    example: '"mocha-coderoad"',
   }, {
     name: 'repository',
     optional: true,
@@ -65,20 +65,20 @@ const pJKeys: PJKeys[] = [{
         repo.hasOwnProperty('url') && typeof repo.url === 'string';
     },
     msg: 'should have a valid repository',
-    example: 'https://github.com/shmck/coderoad-tutorial-name',
+    example: '"https:\/\/github.com/shmck/coderoad-tutorial-name"',
   }, {
     name: 'bugs',
     optional: true,
     validate: (bugs: { url: string }) => typeof bugs === 'object' &&
       bugs.hasOwnProperty('url') && typeof bugs.url === 'string',
     msg: 'should have a link to where to post bugs',
-    example: '"bugs": { "url": "https://github.com/shmck/coderoad-tutorial-name" }'
+    example: '{\n"url": "https:\/\/github.com/shmck/coderoad-tut-name"\n}'
   }, {
     name: 'license',
     optional: true,
     validate: license => typeof license === 'string' && !!license.length,
     msg: 'should have a valid license (ex: MIT, ISC, etc.)',
-    example: 'MIT',
+    example: '"MIT"',
   }];
 
 export default function validatePackageJson(pj: PackageJson): ValidatePjOutput {
