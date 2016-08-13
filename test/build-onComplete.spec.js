c9f4d4a2508a0652099ef43bf0820730884f6e27
ShMcK <shawn.j.mckay@gmail.com>
2016-08-13 14:18:38 -0700

    update onPageComplete tests

--- test/build-onComplete.spec.js
+++ test/build-onComplete.spec.js
@@ -1,48 +1,35 @@
-var expect = require('chai').expect;
-var page = require('../lib/build/parser/page').page;
-var task = require('../lib/build/parser/task').task;
+const { expect }  = require('chai');
+const { page } = require('../lib/build/parser/page');
+const { task } = require('../lib/build/parser/task');
 
-describe('@onPageComplete', function() {
+describe('@onPageComplete', () => {
 
-  var result = function() {
-    return {
-      chapters: [{
-        pages: []
-      }]
-    };
-  };
-  var index = function() {
-    return {
-      chapter: 0,
-      page: -1,
-      task: -1
-    };
-  };
+  const result = () => ({
+    pages: []
+  });
 
-  var resultTask = function() {
-    return {
-      chapters: [{
-        pages: [{
-          title: '',
-          description: '',
-          tasks: []
-        }]
-      }]
-    };
-  };
+  const index = () => ({
+    page: -1,
+    task: -1
+  });
 
-  var indexTask = function() {
-    return {
-      chapter: 0,
-      page: 0,
-      task: -1
-    };
-  };
+  const resultTask = () => ({
+    pages: [{
+      title: '',
+      description: '',
+      tasks: []
+    }]
+  });
 
-  it('should add @onPageComplete string to the page', function() {
-    var lines = ['### Page One', 'page description', 'more page description', '@onPageComplete("next page")'];
-    var next = page(result(), lines, index());
-    var nextPage = next.chapters[0].pages[0];
+  const indexTask = () => ({
+    page: 0,
+    task: -1
+  });
+
+  it('should add @onPageComplete string to the page', () => {
+    const lines = ['## Page One', 'page description', 'more page description', '@onPageComplete("next page")'];
+    const next = page({ result: result(), lines, index: index() });
+    const nextPage = next.pages[0];
     expect(nextPage).to.deep.equal({
       title: 'Page One',
       description: 'page description\nmore page description',
@@ -50,12 +37,12 @@ describe('@onPageComplete', function() {
     });
   });
 
-  it('should handle multi-line codeblocks', function() {
-    var lines = ['### Page One', 'page description', 'more page description',
+  it('should handle multi-line codeblocks', () => {
+    const lines = ['## Page One', 'page description', 'more page description',
       '@onPageComplete("next page', '```', 'var a = 42;', '```', '")'
     ];
-    var next = page(result(), lines, index());
-    var nextPage = next.chapters[0].pages[0];
+    const next = page({ result: result(), lines, index: index() });
+    const nextPage = next.pages[0];
     expect(nextPage).to.deep.equal({
       title: 'Page One',
       description: 'page description\nmore page description',
@@ -63,24 +50,17 @@ describe('@onPageComplete', function() {
     });
   });
 
-  it('should handle string literals', function() {
-    var lines = ['### Page One', 'page description', 'more page description',
+  it('should handle string literals', () => {
+    const lines = ['## Page One', 'page description', 'more page description',
       '@onPageComplete("next page', '`var a = 42;`', '")'
     ];
-    var next = page(result(), lines, index());
-    var nextPage = next.chapters[0].pages[0];
+    const next = page({ result: result(), lines, index: index() });
+    const nextPage = next.pages[0];
     expect(nextPage).to.deep.equal({
       title: 'Page One',
       description: 'page description\nmore page description',
       onPageComplete: 'next page\n`var a = 42;`'
     });
-  });
-
-  it('shouldn\'t add to tasks', function() {
-    var lines = ['+ Task One', 'with more on the next', 'line'];
-    var next = task(resultTask(), lines, indexTask());
-    var nextTask = next.chapters[0].pages[0].tasks[0];
-    expect(nextTask.description).to.equal('Task One\nwith more on the next\nline');
   });
 
 });
